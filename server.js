c2bd5bed98c576994c81152b8cb94b65dec6ec15
Jamyle Elen <jamyleelen09@gmail.com>
2025-01-09 02:23:46 -0300

    fix: allowing cors access

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ import photoRoutes from './src/routes/photoRoutes.js';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json());
 
 app.use('/api', photoRoutes);
